2714b61fc8b24fea1e3b03dd42c98e580a297937
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-06 17:21:45 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyComponent,
+    EmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/company/company.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewChild } from '@angular/core';
+import { EmployeeComponent } from "../employee/employee.component";
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent {
+
+  companyname: string = "ABC Company";
+
+  @ViewChild("second") emp!: EmployeeComponent;
+
+  onClickMeClicked() {
+    console.log(this.emp);
+    this.emp.empname = "Ram";
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
